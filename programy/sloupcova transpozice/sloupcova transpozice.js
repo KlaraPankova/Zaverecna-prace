cd37491b5b1a499acc828308b9eae3dbba0300ce
Klara Pankova <klarapankova2@gmail.com>
2021-11-28 22:42:57 +0100

    rozřazení do sloupců

--- programy/sloupcova transpozice/sloupcova transpozice.js
+++ programy/sloupcova transpozice/sloupcova transpozice.js
@@ -5,21 +5,19 @@ function stranspozice() {
     let delkaklice=klicz.length;
     let textv="";
     let klicv=[ ];
+    let sloupce=[ ];
     let x, a,c,k;
    for(let i=0; i<delkaklice; i++){
-        let klicp=klicz.charCodeAt(i);
-        klicv[i]=klicp;
+        klicv[i]=klicz.charCodeAt(i)
+        sloupce[i]="";
     }
     klicv.sort(function(a, b){return a - b});
     for (i=0;i<delkatextu; i++){
         k=i%delkaklice;
-        if(typeof window["sloupec"+k]=='undefined'){
-            window["sloupec"+k];
-        }
-        window ["sloupec"+k]=window ["sloupec"+k]+textv.charAt(i);
+        sloupce[k]=sloupce[k]+textz.charAt(i);
     }
   /*c=String.fromCharCode(pismeno);
   textv=textv+c;*/
 
-   document.getElementById("vysledek").innerHTML =sloupec0;
+   document.getElementById("vysledek").innerHTML =klicv;
   }
